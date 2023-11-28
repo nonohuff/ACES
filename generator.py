027297acd9fce6d6e86c4d5c2f8f64442f9b64b4
nonohuff <nonohuff@gmail.com>
2023-11-29 07:56:41 +0800

    minor fixes

--- generator.py
+++ generator.py
@@ -4,10 +4,13 @@
 from numpy import random
 
 def generateCliffordCircuit(width, depth, singleGateSet, doubleGateSet):
+    '''This function does ...
+    Inputs: 
+    Outputs: cir - list of ...'''
     # cir = [['X', 'H'], ['CNOT_C', 'CNOT_T'], ['Z', 'I']]
     qubits = list(range(width))
     cir = [['I' for _ in range(width)] for _ in range(depth)]
-    for d in range(width):
+    for d in range(depth):
         if d % 2 == 0:
             for w in range(width):
                 randSingleGate = random.choice(singleGateSet)
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from generator import generateCliffordCircuit
 from calculator import addTwirlingGates
 
-width = 3
-depth = 3
-singleGateSet = ['X', 'H', 'Z', 'I']
+width = 3 # num_qubits
+depth = 3 # number of layers in the circuit (easy + hard)
+singleGateSet = ['X', 'H', 'Z', 'I', 'S']
 doubleGateSet = ['CNOT_C', 'CNOT_T']
-twirlingGateSet = ['X', 'Z', 'I']
+twirlingGateSet = ['X','Y' 'Z', 'I']
 
 # Generate Clifford circuit
 circuit = generateCliffordCircuit(width, depth, singleGateSet, doubleGateSet)
