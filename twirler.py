dcd6398b4afbcc933f0626093ca1d90c3c83de67
e1117166@u.nus.edu <e1117166@u.nus.edu>
2023-11-30 18:03:02 +0800

    Add the noisy model codes.

---
+++ twirler.py
@@ -0,0 +1,52 @@
+from qiskit import QuantumCircuit, quantum_info
+from numpy import random
+
+def CliffordPermute(cliff, pauli):
+    '''This function permutes the pauli operators according to the clifford group, so it computes
+    P' = C P C^T, where C is a clifford operator and P is a pauli operator.
+    Inputs: cliff - A Qiskit Circuit, Clifford, or Gate object.
+            pauli - A Qiskit Pauli object.
+    Outputs: new_pauli - A Qiskit Pauli object.'''
+
+    return pauli.evolve(cliff,frame="s")
+
+def split_circuit_by_barrier(circuit):
+    qasm = circuit.qasm()
+    prelude = []
+    circuits = [[]]
+    for line in qasm.splitlines():
+        if any([line.startswith(t) for t in ['OPENQASM', 'include', 'qreg', 'creg']]):
+            prelude.append(line)
+        elif line.startswith('barrier'):
+            circuits.append([])
+        else:
+            circuits[-1].append(line)
+    circuits_with_prelude = [prelude+circuit for circuit in circuits]
+    for circuit_with_prelude in circuits_with_prelude:
+        yield QuantumCircuit.from_qasm_str('\n'.join(circuit_with_prelude))
+
+
+def GTwirling(circuit):
+    '''This function performs G-twirling on a circuit. It randomly chooses a Pauli from {X,Y,Z,I} and prepends it before a layer, then appends P'=CPC^T after the layer.}
+    Inputs: circuit - A Qiskit Circuit object.
+    Outputs: new_circuit - A Qiskit Circuit object.'''
+    num_qubits = circuit.num_qubits
+    new_circuit = QuantumCircuit(num_qubits)
+    for index, subcircuit in enumerate(split_circuit_by_barrier(circuit)):
+        # if len(subcircuit.data) == 1:
+        #     operated_qubits = [subcircuit.find_bit(q).index for q in subcircuit.data[0].qubits]
+        # else:
+        #     operated_qubits = list(range(num_qubits))
+        pauli_str = ''.join(random.choice(["X","Y","Z","I"]) for _ in range(num_qubits))
+        pauli = quantum_info.Pauli(pauli_str)
+        new_circuit = new_circuit.compose(pauli,qubits=range(num_qubits))
+        new_circuit = new_circuit.compose(subcircuit,qubits=range(num_qubits))
+        new_circuit = new_circuit.compose(CliffordPermute(subcircuit,pauli).to_instruction(),qubits = range(num_qubits))
+        if index != len(list(split_circuit_by_barrier(circuit)))-1:
+            new_circuit.barrier()
+    return new_circuit
+
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,35 @@
+from qiskit import QuantumCircuit, Aer, transpile, assemble
+from qiskit.visualization import plot_histogram
+from qiskit_aer import AerSimulator
+from qiskit.tools.visualization import plot_histogram
+# Import from Qiskit Aer noise module
+from qiskit_aer.noise import (NoiseModel, QuantumError, ReadoutError,
+                              pauli_error, depolarizing_error, thermal_relaxation_error)
+from numpy import random
+from qiskit.circuit.library import XGate, YGate, ZGate, HGate, SGate, TGate, CXGate, IGate
+
+
+def IMatrix(): return IGate().to_matrix()
+
+
+def XMatrix(): return XGate().to_matrix()
+
+
+def YMatrix(): return YGate().to_matrix()
+
+
+def ZMatrix(): return ZGate().to_matrix()
+
+
+def HMatrix(): return HGate().to_matrix()
+
+
+def SMatrix(): return SGate().to_matrix()
+
+
+def TMatrix(): return TGate().to_matrix()
+
+
+# def CXMatrix(): return CXGate().to_matrix()
+
+
